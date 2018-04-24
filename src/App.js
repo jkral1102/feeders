7a3c1387281d7dfb10327124796d13616f267b1b
hakpan <hakpan@umn.edu>
2018-04-23 20:32:23 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+
+//importing router for multiple pages
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from './Components/Pages/home';
+import UserHomePage from './Components/Pages/userHome';
+import AboutPage from './Components/Pages/about';
+import SettingsPage from './Components/Pages/settings';
+import SignupPage from './Components/Pages/signup';
+
+
 // All components 
 import Cards from "./Components/Cards";
 import Navbar from "./Components/Navbar";
@@ -9,15 +19,29 @@ import Twitter from "./Components/Twitter";
 class App extends Component {
   render() {
     return (
+      //creating router to have multiple pages
+      <Router>
+        <div>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/userHome" component={UserHomePage} />
+          <Route path="/settings" component={SettingsPage} />
+          <Route path="/signup" component={SignupPage} />
+          <Route component={NoMatch} />
+        </div>
+      </Router>
+
       
-      <div className="App">
-        <Navbar /> 
-        <Cards />
-        <Twitter/>
-        <Footer/>
-      </div>
     );
   }
 }
+
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>
+      No match for <code>{location.pathname}</code>
+    </h3>
+  </div>
+);
 
 export default App;
---
+++ src/Components/Pages/settings.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+class SettingsPage extends Component {
+
+    render() {
+    return (
+      <div className="App">
+      <h1> Settings Page!!</h1>
+        
+          
+      </div>
+    );
+  }
+}
+
+export default SettingsPage;
