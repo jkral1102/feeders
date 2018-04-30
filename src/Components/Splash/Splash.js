47f0d6f79e7245a94837dd67084ff73c6da51eb3
unknown <jkral1101@gmail.com>
2018-04-30 18:56:19 -0500

    refactoring to set 3 pages - move splash from a page intoa  component

---
+++ src/Components/Splash/Splash.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import './Splash.css'
+import Login from "../Login";
+import "./particles.json";
+
+
+
+class Splash extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isToggleOn: false,
+        };
+    
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+      }
+    
+      handleClick() {
+        this.setState({
+          isToggleOn: !this.state.isToggleOn
+        });
+      }
+   
+
+  render() {
+   
+    return (
+      <div className="splash">
+      <div className="row">
+        <div className="col-12 size">
+          {/* <h1>Header</h1> */}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-1">
+        
+        </div>
+        <div className="col-5">
+          <div> 
+            <h2>FEED ME</h2>
+            <p> An App for all your social media.</p> </div>
+        </div>
+        <div className="col-5">
+
+          <a href="#" className="link" onClick={this.handleClick}>
+              Login
+          </a>
+          <a href="#" className="link"> Sign Up </a>
+        
+        </div>
+        <div className="col-1">
+        
+        </div>
+      </div>
+        <div className="row"> 
+        <div className="col-12 size">
+        
+        </div>
+        </div>
+        <Login open={this.state.isToggleOn} />
+      </div>
+    );
+  }
+}
+
+
+export default Splash;
