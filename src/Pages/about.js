c653cb1d9ab66a49f13a7e2c887507482f1d7909
OpenPier <35052947+OpenPier@users.noreply.github.com>
2018-04-30 21:30:44 -0500

    1.0.0

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -8,9 +8,37 @@ class AboutPage extends Component {
     return (
       <div className="App">
       <Navbar />
-      <h1> About us page!!</h1>
-        
-          <Footer />
+
+
+      {/* The purpose of Feeders */}
+      <h1> About Our Team!!</h1>
+        <p>There are hundreds of social media sites on the menu. With Feeders you don't have to keep getting 
+          bothered by all those alerts on your phone and going into all those apps. 
+          You wouldn't eat the ingrediants of a meal separately, would you? <br></br>
+
+          Feeders is to serve you ALL your favorite social media sites into one bite.
+        </p>
+
+
+          {/* Team cards */}
+          {/* <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            
+          />
+        ))}
+          </Wrapper> */}
+
+
+
+          <Footer/>
       </div>
     );
   }
